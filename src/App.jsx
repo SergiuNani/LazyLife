4e751a0610325d6f8c0e3b3faa1afe2b48cb0edc
SergiuNani <130356254+SergiuNani@users.noreply.github.com>
2024-07-01 19:49:21 +0200

    0070101

--- src/App.jsx
+++ src/App.jsx
@@ -7,59 +7,59 @@ import { renderHtmlContent } from "./AppHelper";
 import { DocxTemplaterX } from "./DocxTemplater";
 
 function App() {
-  const [htmlContent, setHtmlContent] = useState([]);
-  const firstMount = useRef(false);
-  // const [htmlContent, setHtmlContent] = useState(
-  //   ExtractUsefulInfo(HTML_original)
-  // );
+    // const [htmlContent, setHtmlContent] = useState([]);
+    const firstMount = useRef(false);
+    const [htmlContent, setHtmlContent] = useState(
+        ExtractUsefulInfo(HTML_original)
+    );
 
-  useEffect(() => {
-    const handleKeyPress = (event) => {
-      if (event.ctrlKey && event.key.toLowerCase() === "q") {
-        // DocxTemplaterX(htmlContent);
-      }
+    useEffect(() => {
+        const handleKeyPress = (event) => {
+            if (event.ctrlKey && event.key.toLowerCase() === "q") {
+                // DocxTemplaterX(htmlContent);
+            }
+        };
+        window.addEventListener("keydown", handleKeyPress);
+        return () => {
+            window.removeEventListener("keydown", handleKeyPress);
+        };
+    }, []);
+
+    useEffect(() => {
+        if (firstMount.current) {
+            DocxTemplaterX(htmlContent);
+        } else {
+            firstMount.current = true;
+        }
+    }, [htmlContent]);
+    const handleClick = () => {
+        chrome.runtime.sendMessage({ action: "getHTML" }, (response) => {
+            const bodyHTML = response.bodyHTML;
+            // setHtmlContent(bodyHTML);
+            setHtmlContent(ExtractUsefulInfo(bodyHTML));
+        });
     };
-    window.addEventListener("keydown", handleKeyPress);
-    return () => {
-      window.removeEventListener("keydown", handleKeyPress);
-    };
-  }, []);
 
-  useEffect(() => {
-    if (firstMount.current) {
-      DocxTemplaterX(htmlContent);
-    } else {
-      firstMount.current = true;
-    }
-  }, [htmlContent]);
-  const handleClick = () => {
-    chrome.runtime.sendMessage({ action: "getHTML" }, (response) => {
-      const bodyHTML = response.bodyHTML;
-      // setHtmlContent(bodyHTML);
-      setHtmlContent(ExtractUsefulInfo(bodyHTML));
-    });
-  };
-
-  return (
-    <>
-      <div>
-        <h1>Lazy Life</h1>
-        <img src={Drunk} className="logo" alt="Vite logo" />
-      </div>
-      <div className="card">
-        <button onClick={handleClick} style={{ fontSize: "1.2rem" }}>
-          CLICK ME
-        </button>
-        {/* <button
-          onClick={() => {
-            DocxTemplaterX(ExtractUsefulInfo(HTML_original));
-          }}
-        >
-          Demo
-        </button> */}
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div>
+                <h1>Lazy Life</h1>
+                <img src={Drunk} className="logo" alt="Logo image here" />
+            </div>
+            <div className="card">
+                <button onClick={handleClick} style={{ fontSize: "1.2rem" }}>
+                    CLICK ME
+                </button>
+                <button
+                    onClick={() => {
+                        DocxTemplaterX(ExtractUsefulInfo(HTML_original));
+                    }}
+                >
+                    Demo
+                </button>
+            </div>
+        </>
+    );
 }
 
 export default App;
